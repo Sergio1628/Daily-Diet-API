64448d84848028e7a48c573b7de62f44b7c6f23c
Sergio <sergio.lss1@hotmail.com>
2025-02-21 16:27:34 -0300

    Adding Update refeiçao

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1
 
 db.init_app(app)
 
+VALID_FIELDS = {"plate_name", "weight", "description", "diet"}
+
 # Create
 @app.route('/refeicao', methods=['POST'])
 def create_refeicao():
@@ -18,8 +20,9 @@ def create_refeicao():
     weight = data.get("weight")
     description = data.get("description")
     diet = data.get("diet")
+    refeicao = [plate_name, description, weight]
 
-    if plate_name and description and weight and diet:
+    if all (refeicao) and diet is not None:
         refeicao = Refeicao(plate_name=plate_name, description=description, weight=weight, diet=diet)
         db.session.add(refeicao)
         db.session.commit()
@@ -35,6 +38,40 @@ def read_refeicao(id_refeicao):
         return {"plate_name": refeicao.plate_name}
 
     return jsonify({"mensagem": "Refeição não encontrada!"}), 404
+
+
+@app.route('/refeicao/<int:id_refeicao>', methods=['PATCH'])
+def update_refeicao(id_refeicao):
+    data = request.json
+    refeicao = Refeicao.query.get(id_refeicao)
+    
+    # Valida se o id da refeiçao existe
+    if not refeicao:
+        return jsonify({"mensagem": "Erro! Refeição não encontrada."}), 404 
+    # Valida se pelo menos um dos campos validos existe na requisição
+    if not any(key in data for key in VALID_FIELDS):
+        return jsonify({"mensagem": "Nenhum dado válido enviado."}), 400
+
+    # Percorre todos os campos válidos e substitui dinamicamente com os novos dados
+    for field in VALID_FIELDS:
+        if field in data:
+            setattr(refeicao, field, data[field])
+
+    db.session.commit()
+
+    return jsonify ({"mensagem": f"Refeição {id_refeicao} atualizada com sucesso!"})
+    
+           
+
+
+''' Bloco funcional, mas como PUT, que exige todos os parametros da var refeicao no payload
+    if refeicao:
+        refeicao.plate_name = data.get("plate_name")
+        refeicao.weight = data.get("weight")
+        refeicao.description = data.get("description")
+        refeicao.diet = data.get("diet")
+        
+        db.session.commit()'''
 
 if __name__ == '__main__':
     app.run(debug=True)
